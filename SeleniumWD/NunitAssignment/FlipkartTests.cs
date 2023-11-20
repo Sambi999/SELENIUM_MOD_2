2074467323222002e67e7da011e2dde9b852fb55
Sambi999 <206217@ust.com>
2023-11-20 22:37:37 +0530

    firstcommit

---
+++ SeleniumWD/NunitAssignment/FlipkartTests.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitAssignment
+{
+    internal class FlipkartTests:   Corecode
+    {
+        [Test]
+        [Description("Check for search box")]
+        public void SearchBoxTest()
+        {
+            DefaultWait<IWebDriver> fluentWait = new DefaultWait<IWebDriver>(driver);
+            fluentWait.Timeout = TimeSpan.FromSeconds(5);
+            fluentWait.PollingInterval = TimeSpan.FromMilliseconds(100);
+            fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            fluentWait.Message = "Element Not Found";
+
+            driver.FindElement(By.XPath("/html/body/div[3]/div/span")).Click();
+            IWebElement searchInput = fluentWait.Until(driv => driv.FindElement(By.ClassName("Pke_EE")));
+
+            searchInput.SendKeys("laptops");
+            searchInput.SendKeys(Keys.Enter);
+        }
+    }
+}
---
+++ SeleniumWD/NunitAssignment/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace NunitAssignment
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
