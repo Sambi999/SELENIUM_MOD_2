a79f29c255eb68ca75639a93b212ed850d8cb353
Sambi999 <206217@ust.com>
2023-11-24 07:19:08 +0530

    firstcommit

---
+++ SeleniumWD/BunnyCart/PageObjects/ProductPage.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunnyCart.PageObjects
+{
+    internal class ProductPage
+    {
+        
+        IWebDriver driver;
+        public ProductPage(IWebDriver? driver)
+        {
+            this.driver = driver ?? throw new ArgumentException(nameof(driver));
+            PageFactory.InitElements(driver, this);
+            
+        }
+        [FindsBy(How = How.XPath, Using = " //h1[@class='page-title']")]
+        private IWebElement? ProductTitleLabel { get; }
+
+        [FindsBy(How = How.ClassName, Using = " qty-inc")]
+        private IWebElement? IncQtyLink { get; }
+
+        [FindsBy(How = How.Id, Using = " product-addtpocart-button")]
+        private IWebElement?AddToCartBtn { get; set; }
+
+        public string? GetProductTitleLabel()
+        {
+            return ProductTitleLabel?.Text;
+        }
+        public void ClickIncQtyLink()
+        {
+            IncQtyLink?.Click();
+        }
+        public void ClickAddToCartBtn()
+        {
+            AddToCartBtn?.Click();
+        }
+
+
+
+
+
+    }
+    
+}
+
---
+++ SeleniumWD/BunnyCart/TestScripts/BunnyCartTests.cs
@@ -0,0 +1,35 @@
+﻿using BunnyCart.PageObjects;
+using BunnyCart.Utilities;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunnyCart.TestScripts
+{
+    [TestFixture]
+    internal class BunnyCartTests : Corecode
+    {
+        [Test]
+        public void SignUpTest()
+        {
+            BunnyCartHomePage bunnyCartHomePage = new BunnyCartHomePage(driver);
+            bunnyCartHomePage.ClickCreateAccountLink();
+            Thread.Sleep(1000);
+            try
+            {
+                Assert.That(driver.FindElement(By.XPath("//div[" + "@class='modal-inner-wrap']//following::h1[2]")).Text,
+                    Is.EqualTo("Create an Account"));
+                bunnyCartHomePage.ClickCreateAccountButton("raj", "kumar", "raj@gmail.com", "12345", "12345", "9876543423");
+            }
+            catch (AssertionException)
+            {
+                Console.WriteLine("Sign Up failed");
+            }
+
+        }
+        
+    }
+}
