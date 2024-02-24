e6192b64e9a8113e07fbde427d6be3610bb02343
Sambi999 <148055654+Sambi999@users.noreply.github.com>
2024-02-24 21:56:44 +0530

    Add files via upload

---
+++ SearchDataRead.cs
@@ -0,0 +1,59 @@
+﻿using ExcelDataReader;
+using Google.Test_Data_Classes;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.Utilities
+{
+    public class SearchDataRead
+    {
+        public static List<SearchData> ReadSearchText(string excelFilePath, string sheetName)
+        {
+            List<SearchData> excelDataList = new List<SearchData>();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            using (var stream = File.Open(excelFilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                    {
+                        ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                        {
+                            UseHeaderRow = true,
+                        }
+                    });
+                    var dataTable = result.Tables[sheetName];
+                    if (dataTable != null)
+
+                    {
+
+                        foreach (DataRow row in dataTable.Rows)
+                        {
+                            SearchData excelData = new SearchData()
+                            {
+                                SearchText = GetValueOrDefault(row, "search-text")
+                            };
+                            excelDataList.Add(excelData);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Sheet '{sheetName}' not found in the Excel file.");
+                    }
+                    
+                }
+            }
+            return excelDataList;
+        }
+        static string? GetValueOrDefault(DataRow row, string columnName)
+        {
+            return row.Table.Columns.Contains(columnName) ? row[columnName]?.ToString() : null;
+        }
+    }
+}
